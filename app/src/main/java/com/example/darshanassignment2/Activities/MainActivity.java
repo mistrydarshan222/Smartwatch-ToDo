11b6e4cc865a473a08fabd55f85f20befc9748ad
mistrydarshan222 <69639961+mistrydarshan222@users.noreply.github.com>
2025-04-02 13:06:19 -0400

    voice input done

--- app/src/main/java/com/example/darshanassignment2/Activities/MainActivity.java
+++ app/src/main/java/com/example/darshanassignment2/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.pm.PackageManager;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "onCreate started");
         setContentView(R.layout.activity_main);
 
         btnAddTask = findViewById(R.id.btnAddTask);
